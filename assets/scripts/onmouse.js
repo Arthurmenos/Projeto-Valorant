dd999b7a40fe90fb8ee5e484b72b3396b0f97360
Arthur Oliveira <thenetherarthur@gmail.com>
2022-07-22 22:44:21 -0300

    V0.2

---
+++ assets/scripts/onmouse.js
@@ -0,0 +1,7 @@
+function MEnterAstra() {
+    document.querySelector('img#astra').src = "assets/images/artwork/Astra.webp"
+}
+
+function MOutAstra() {
+    document.querySelector('img#astra').src = "assets/images/portrait/Astra.webp"
+}
